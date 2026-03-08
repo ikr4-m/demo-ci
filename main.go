90d4bcb7cd60acde8a2a8670f8487a43d0784fd4
Ikramullah Latif <contact@ikr4m.my.id>
2026-03-08 09:57:14 +0700

    Add crash endpoint

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,6 +19,12 @@ func main() {
 			http.StatusOK,
 			fmt.Sprintf("Hello world from %s revision!", versionNumber),
 		)
+	})
+
+	e.GET("/crash", func(c echo.Context) error {
+		log.Println("Calling crash url")
+		os.Exit(1)
+		return c.String(http.StatusOK, "Lmaooo")
 	})
 
 	if err := e.Start(":8080"); err != nil {
